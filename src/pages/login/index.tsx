874580ec5f3b94be27b3abdb6892a421c7011ed1
Moacir <moacir9401@gmail.com>
2021-04-12 19:37:37 -0300

    correção login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -18,12 +18,10 @@ const Login: React.FC = () => {
               <IonLabel position="floating">Senha</IonLabel>
               <IonInput></IonInput>
             </IonItem>
-            <p>Esqueci a senha</p>
-<<<<<<< HEAD
-            <IonButton expand="block" routerLink="/main">Entrar</IonButton>
-=======
+            <p>Esqueci a senha</p> 
+            
             <IonButton expand="block" size="large" routerLink="/main">Entrar</IonButton>
->>>>>>> e0e733c0c430c8eac1fb54e9a409006de51781b7
+
             <p>
               Não possui conta? <Link to="/registerUser">Cadastre-se</Link> agora!
             </p>
