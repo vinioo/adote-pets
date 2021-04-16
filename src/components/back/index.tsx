c243c2084ec9ce9823826db9c99d5b150fe46011
Vinicius Oliveira Souza <viniciusos@take.net>
2021-04-15 21:25:20 -0300

    fix pet detail styling

---
+++ src/components/back/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import BackIcon from "../../assets/icons/back.svg";
+
+import "./Back.css";
+
+const Back: React.FC = () => {
+  return <img src={BackIcon} alt="return page" className="back"></img>;
+};
+
+export default Back;
--- src/pages/pet-info/index.tsx
+++ src/pages/pet-info/index.tsx
@@ -1,6 +1,8 @@
 import { IonButton, IonContent, IonPage } from "@ionic/react";
 import React from "react";
 
+import Back from "../../components/back";
+
 import "./PetInfo.css";
 
 const PetInfo: React.FC = () => {
@@ -8,11 +10,13 @@ const PetInfo: React.FC = () => {
     <IonPage>
       <IonContent fullscreen>
         <div className="info">
-          <div className="info__image"></div>
+          <div className="info__image">
+            <Back></Back>
+          </div>
           <div className="info__content">
             <div className="info__content__pet">
               <div className="info__content__pet--left">
-                <p>Doguinho</p>
+                <h3>Doguinho</h3>
                 <span>Cachorro</span>
               </div>
               <div className="info__content__pet--right">
@@ -24,14 +28,16 @@ const PetInfo: React.FC = () => {
               <div className="info__content__user__description">
                 <div className="info__content__user__description__image"></div>
                 <h3>Josiane silva</h3>
-                <p>2.5km</p>
+                <div className="info__content__user__description__distance">
+                  <p>2.5km</p>
+                </div>
               </div>
               <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, molestiae. Unde, exercitationem. Aliquam
                 quis omnis veniam quas illum optio quo, voluptas architecto eos esse. Amet eius rem voluptates minus
                 pariatur!
               </p>
-              <IonButton>Entrar em contato</IonButton>
+              <IonButton expand="block">Entrar em contato</IonButton>
             </div>
           </div>
         </div>
