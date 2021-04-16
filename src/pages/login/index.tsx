ef06f4ce161fd3342372209ff8b348853c8e3a74
Vinicius Oliveira Souza <viniciusos@take.net>
2021-04-15 21:46:51 -0300

    add formik to login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,18 @@
 import { IonContent, IonPage, IonButton, IonInput, IonItem, IonLabel } from "@ionic/react";
 import { Link } from "react-router-dom";
 
+import { Formik, Form } from "formik";
+
 import "./Login.css";
 
+interface MyFormValues {
+  email: string;
+  password: string;
+}
+
 const Login: React.FC = () => {
+  const initialValues: MyFormValues = { email: "", password: "" };
+  
   return (
     <IonPage>
       <IonContent fullscreen>
@@ -11,18 +20,38 @@ const Login: React.FC = () => {
           <div className="login__content">
             <h1>Login</h1>
             <div>
-              <IonItem>
-                <IonLabel position="floating">Email</IonLabel>
-                <IonInput></IonInput>
-              </IonItem>
-              <IonItem>
-                <IonLabel position="floating">Senha</IonLabel>
-                <IonInput></IonInput>
-              </IonItem>
-              <small>Esqueci a senha</small>
-            <IonButton expand="block" size="large" routerLink="/main">
-              Entrar
-            </IonButton>
+              <Formik
+                initialValues={initialValues}
+                onSubmit={(values, actions) => {
+                  console.log({ values, actions });
+                  alert(JSON.stringify(values, null, 2));
+                  actions.setSubmitting(false);
+                }}
+              >
+                {(formikProps) => (
+                  <Form>
+                    <IonItem>
+                      <IonLabel position="floating">Email</IonLabel>
+                      <IonInput
+                        name="email"
+                        onIonChange={(e) => formikProps.setFieldValue("email", e.detail.value)}
+                      ></IonInput>
+                    </IonItem>
+                    <IonItem>
+                      <IonLabel position="floating">Senha</IonLabel>
+                      <IonInput
+                        name="password"
+                        type="password"
+                        onIonChange={(e) => formikProps.setFieldValue("password", e.detail.value)}
+                      ></IonInput>
+                    </IonItem>
+                    <small>Esqueci a senha</small>
+                    <IonButton expand="block" size="large" type="submit">
+                      Entrar
+                    </IonButton>
+                  </Form>
+                )}
+              </Formik>
             </div>
             <small>
               Não possui conta? <Link to="/registerUser">Cadastre-se</Link> agora!
