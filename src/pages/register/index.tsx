93d066d7dfdac84b77f6dbb743c86ccb54991247
moacir alves <moacir9401@gmail.com>
2021-04-18 20:19:58 -0300

    implementação das paginas do registro com o Formik e upload de imagem base64

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -6,39 +6,123 @@ import {
   IonInput,
   IonItem,
   IonLabel,
+  IonImg,
 } from "@ionic/react";
 import { Link } from "react-router-dom";
 import { add, arrowBackOutline } from "ionicons/icons";
+import { Formik, Form } from "formik";
 
 import "./Register.css";
+import { createRef } from "react";
+
+interface MyFormValues {
+  tipoAnimal: string;
+  nome: string;
+  idade: number;
+  imagem: string;
+}
 
 const Register: React.FC = () => {
+  const initialValues: MyFormValues = {
+    tipoAnimal: "",
+    nome: "",
+    idade: 0,
+    imagem: "",
+  };
+  var style_Image_upload = {};
+  var style_Icon_upload = {};
+
   return (
     <IonPage>
       <IonContent fullscreen>
         <div className="container_main">
-        <Link to="/main">   <IonIcon icon={arrowBackOutline}></IonIcon></Link>
-          <div className="image_upload_content">
-            <div className="image_upload">
-              <IonIcon icon={add} className="icon"></IonIcon>
-            </div>
-            <small className="image_upload_subtitle">adicionar uma imagem</small>
-          </div>
-          <IonItem>
-            <IonLabel position="floating">Tipo de animal</IonLabel>
-            <IonInput></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="floating">Nome</IonLabel>
-            <IonInput></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="floating">Idade</IonLabel>
-            <IonInput></IonInput>
-          </IonItem>
-          <IonButton expand="block" className="button" size="large">
-            Colocar para adoção
-          </IonButton>
+          <Link to="/main">
+            {" "}
+            <IonIcon icon={arrowBackOutline}></IonIcon>
+          </Link>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={(values, action) => {
+              alert(JSON.stringify(values, null, 2));
+              action.setSubmitting(false);
+            }}
+          >
+            {(formikProps) => (
+              <Form>
+                <div className="image_upload_content">
+                  <div className="image_upload" style={style_Image_upload}>
+                    <IonIcon icon={add} className="icon" style={style_Icon_upload}></IonIcon>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      className="upload_file"
+                      name="imagem"
+                      onChange={(e) => {
+                        const file = e.target.files
+                          ? e.target.files[0]
+                          : new Blob();
+                        const reader = new FileReader();
+                        reader.onload = (ee) => {
+                          formikProps.setFieldValue(
+                            "imagem",
+                            ee.target?.result?.toString()
+                          );
+                          style_Image_upload = {
+                            backgroundImage: `url('${ee.target?.result?.toString()}')`,
+                            backgroundZize: "1%",
+                          };
+
+                          style_Icon_upload = {
+                            display: "none",
+                          };
+                        };
+
+                        reader.readAsDataURL(file);
+                      }}
+                    ></input>
+                  </div>
+                  <small className="image_upload_subtitle">
+                    adicionar uma imagem
+                  </small>
+                </div>
+                <IonItem>
+                  <IonLabel position="floating">Tipo de animal</IonLabel>
+                  <IonInput
+                    name="tipoAnimal"
+                    onIonChange={(e) => {
+                      formikProps.setFieldValue("tipoAnimal", e.detail.value);
+                    }}
+                  ></IonInput>
+                </IonItem>
+                <IonItem>
+                  <IonLabel position="floating">Nome</IonLabel>
+                  <IonInput
+                    name="nome"
+                    onIonChange={(e) => {
+                      formikProps.setFieldValue("nome", e.detail.value);
+                    }}
+                  ></IonInput>
+                </IonItem>
+                <IonItem>
+                  <IonLabel position="floating">Idade</IonLabel>
+                  <IonInput
+                    name="idade"
+                    onIonChange={(e) => {
+                      formikProps.setFieldValue("idade", e.detail.value);
+                    }}
+                  ></IonInput>
+                </IonItem>
+                <IonButton
+                  expand="block"
+                  className="button"
+                  size="large"
+                  type="submit"
+                >
+                  Colocar para adoção
+                </IonButton>
+              </Form>
+            )}
+          </Formik>
         </div>
       </IonContent>
     </IonPage>
--- src/pages/registerUser/index.tsx
+++ src/pages/registerUser/index.tsx
@@ -1,42 +1,133 @@
-import {IonIcon, IonContent, IonPage, IonButton, IonInput, IonItem, IonLabel } from "@ionic/react";
-import { arrowBackOutline,add } from 'ionicons/icons';
+import {
+  IonIcon,
+  IonContent,
+  IonPage,
+  IonButton,
+  IonInput,
+  IonItem,
+  IonLabel,
+} from "@ionic/react";
+import { arrowBackOutline, add } from "ionicons/icons";
 import { Link } from "react-router-dom";
-
+import { Formik, Form } from "formik";
 
 import "./RegisterUser.css";
 
+interface MyFormValues {
+  email: string;
+  password: string;
+  telefone: string;
+}
+
 const RegisterUser: React.FC = () => {
+  const initialValues: MyFormValues = {
+    email: "",
+    password: "",
+    telefone: "",
+  };
+  var style_Image_upload = {};
+  var style_Icon_upload = {};
+
   return (
     <IonPage>
       <IonContent fullscreen>
         <div className="container_main">
-        <Link to="/main">   <IonIcon icon={arrowBackOutline}></IonIcon></Link>
-        <h1 className="title">Registrar</h1>
-          <div className="image_upload_content">
-            <div className="image_upload">
-              <IonIcon icon={add} className="icon"></IonIcon>
-            </div>
-            <small className="image_upload_subtitle">adicionar uma imagem</small>
-          </div>
-          <IonItem>
-            <IonLabel position="floating">Email</IonLabel>
-            <IonInput></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="floating">Senha</IonLabel>
-            <IonInput></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="floating">Confirmação de senha</IonLabel>
-            <IonInput></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="floating">Número de telefone</IonLabel>
-            <IonInput></IonInput>
-          </IonItem>
-          <IonButton expand="block" className="button" size="large" routerLink="/main">
-            Cadastrar
-          </IonButton>
+          <Link to="/login">
+            {" "}
+            <IonIcon icon={arrowBackOutline}></IonIcon>
+          </Link>
+          <h1 className="title">Registrar</h1>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={(values, action) => {
+              alert(JSON.stringify(values, null, 2));
+              action.setSubmitting(false);
+            }}
+          >
+            {(formikProps) => (
+              <Form>
+                <div className="image_upload_content">
+                  <div className="image_upload" style={style_Image_upload}>
+                    <IonIcon
+                      icon={add}
+                      className="icon"
+                      style={style_Icon_upload}
+                    ></IonIcon>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      className="upload_file"
+                      name="imagem"
+                      onChange={(e) => {
+                        const file = e.target.files
+                          ? e.target.files[0]
+                          : new Blob();
+                        const reader = new FileReader();
+                        reader.onload = (ee) => {
+                          formikProps.setFieldValue(
+                            "imagem",
+                            ee.target?.result?.toString()
+                          );
+                          style_Image_upload = {
+                            backgroundImage: `url('${ee.target?.result?.toString()}')`,
+                            backgroundZize: "1%",
+                          };
+
+                          style_Icon_upload = {
+                            display: "none",
+                          };
+                        };
+
+                        reader.readAsDataURL(file);
+                      }}
+                    ></input>
+                  </div>
+                  <small className="image_upload_subtitle">
+                    adicionar uma imagem
+                  </small>
+                </div>
+                <IonItem>
+                  <IonLabel position="floating">Email</IonLabel>
+                  <IonInput
+                    name="email"
+                    onIonChange={(e) => {
+                      formikProps.setFieldValue("email", e.detail.value);
+                    }}
+                  ></IonInput>
+                </IonItem>
+                <IonItem>
+                  <IonLabel position="floating">Senha</IonLabel>
+                  <IonInput
+                    name="password"
+                    onIonChange={(e) => {
+                      formikProps.setFieldValue("password", e.detail.value);
+                    }}
+                  ></IonInput>
+                </IonItem>
+                <IonItem>
+                  <IonLabel position="floating">Confirmação de senha</IonLabel>
+                  <IonInput name="confirmarSenha"></IonInput>
+                </IonItem>
+                <IonItem>
+                  <IonLabel position="floating">Número de telefone</IonLabel>
+                  <IonInput
+                    name="telefone"
+                    onIonChange={(e) => {
+                      formikProps.setFieldValue("telefone", e.detail.value);
+                    }}
+                  ></IonInput>
+                </IonItem>
+                <IonButton
+                  expand="block"
+                  className="button"
+                  size="large" 
+                  type="submit"
+                >
+                  Cadastrar
+                </IonButton>
+              </Form>
+            )}
+          </Formik>
         </div>
       </IonContent>
     </IonPage>
